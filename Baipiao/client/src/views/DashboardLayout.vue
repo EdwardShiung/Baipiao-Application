2649b42e6fc5aae39c988e8d724774cc1aa86080
EdwardShiung <edwardshiung@gmail.com>
2024-10-29 06:19:05 -0400

    Update Dashboard Layout 1. Setting the Routes and Authorization in Dashboard Layout 2. Setting the Menue by Permission Role

--- Baipiao/client/src/views/DashboardLayout.vue
+++ Baipiao/client/src/views/DashboardLayout.vue
@@ -1,53 +1,115 @@
 <template>
-    <div class="wrapper">
-      <side-bar>
-        <template slot="links">
-          <sidebar-link to="/" name="Baipiao Events" icon="ti-calendar" />
-          <sidebar-link to="/baipiaoEvents" name="Events" icon="ti-map" />
-          <sidebar-link to="/baipiaouser" name="Users" icon="ti-user" />
-          <sidebar-link to="/baipiaoOrganization" name="Organizations" icon="ti-briefcase" />
-          <sidebar-link to="/baipiaoVenue" name="Venues" icon="ti-location-pin" />
-          <sidebar-link to="/baipiaoCategory" name="Event Categories" icon="ti-tag" />
-        </template>
-<!-- Mobile Side Bar -->
-<mobile-menu>
-
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link to="/" name="Baipiao Events" icon="ti-calendar" />
+        <sidebar-link
+          v-for="item in filteredMenuItems"
+          :key="item.name"
+          :to="item.path"
+          :name="item.name"
+          :icon="item.icon"
+        />
+      </template>
+      <!-- Mobile Side Bar -->
+      <mobile-menu>
         <li class="nav-item">
-          <a class="nav-link">
+          <a class="nav-link" @click.prevent="logout">
             <i class="ti-settings"></i>
             <p>Logout</p>
           </a>
         </li>
         <li class="divider"></li>
       </mobile-menu>
-
-      </side-bar>
-      <div class="main-panel">
-        <top-navbar></top-navbar>
-        <dashboard-content @click.native="toggleSidebar"> </dashboard-content>
-        <content-footer></content-footer>
-      </div>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+      <dashboard-content @click.native="toggleSidebar"> </dashboard-content>
+      <content-footer></content-footer>
     </div>
-  </template>
+  </div>
+</template>
   <style lang="scss"></style>
-  <script>
-  import TopNavbar from "../components/TopNavBar/TopNavbar.vue";
-  import DashboardContent from "../views/Content.vue";
-  import MobileMenu from "./MobileMenu";
-  import ContentFooter from "./ContentFooter.vue";
-  export default {
-    components: {
-      TopNavbar,
-      DashboardContent,
-      MobileMenu,
-      ContentFooter,
+  
+<script>
+import TopNavbar from "../components/TopNavBar/TopNavbar.vue";
+import DashboardContent from "../views/Content.vue";
+import MobileMenu from "./MobileMenu";
+import ContentFooter from "./ContentFooter.vue";
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+    MobileMenu,
+    ContentFooter,
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          name: "Events",
+          path: "/baipiaoEvents",
+          icon: "ti-map",
+          requiredUserTypes: ['admin', 'organization', 'user'], 
+        },
+        {
+          name: "Baipiao list",
+          path: "/baipiaolist",
+          icon: "ti-map",
+          requiredUserTypes: ['admin'], 
+        },
+        {
+          name: "Users",
+          path: "/baipiaouser",
+          icon: "ti-user",
+          requiredUserTypes: ['admin'],
+        },
+        {
+          name: "Organizations",
+          path: "/baipiaoOrganization",
+          icon: "ti-briefcase",
+          requiredUserTypes: ['admin'],
+        },
+        {
+          name: "Venues",
+          path: "/baipiaoVenue",
+          icon: "ti-location-pin",
+          requiredUserTypes: ['admin', 'organization'],
+        },
+        {
+          name: "Event Categories",
+          path: "/baipiaoCategory",
+          icon: "ti-tag",
+          requiredUserTypes: ['admin'],
+        },
+      ],
+    };
+  },
+  computed: {
+    filteredMenuItems() {
+      const userType = this.getUserType(); 
+      return this.menuItems.filter(item => {
+        if (!item.requiredUserTypes) return true;
+        return item.requiredUserTypes.includes(userType);
+      });
     },
-    methods: {
-      toggleSidebar() {
-        if (this.$sidebar.showSidebar) {
-          this.$sidebar.displaySidebar(false);
-        }
-      },
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
     },
-  };
-  </script>
+    getUserType() {
+      const userType = localStorage.getItem("token");
+      return userType ? JSON.parse(userType) : null;
+    },
+    logout() {
+      // Clear localStorage
+      localStorage.removeItem("token");
+      // Redirect to Login
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
